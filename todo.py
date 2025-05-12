6142c404dec997524e50e952bd9f31ce0e6550ea
Zuhal Sakhi Zada <zuhalsakhizada3@gmail.com>
2025-05-12 22:59:42 +0200

    to_do_list

--- todo.py
+++ todo.py
@@ -8,7 +8,55 @@ def show_menu():
     print("1. View tasks")
     print("2. Add a task")
     print("3. Remove a task")
-    print("4. Exit\n")
+    print("4. mark task as completed")
+    print("5. Exit\n")
+
+def view_tasks():
+    if not tasks:
+        time.sleep(0.3)
+        print("No tasks yet!")
+    else:
+        time.sleep(1)
+        print("\nYour tasks:")
+        for i, task in enumerate(tasks, start=1):
+            status = "[x]" if task["done"] else "[ ]"
+            print(f"{i}. {status} {task['text']}")
+
+def add_task():
+    new_task = input("Add a new task: ")
+    tasks.append({"text": new_task, "done": False})
+    time.sleep(1)
+    print("Task added!")
+
+def remove_task():
+        time.sleep(0.3)
+        view_tasks()
+        try:
+            time.sleep(1)
+            delete_task = int(input("Enter the number of the task you want to delete: "))
+            if 1 <= delete_task <= len(tasks):
+                removed = tasks.pop(delete_task - 1)
+                time.sleep(1)
+                print(f"Removed: {removed}")
+            else:
+                print("invalid task number")
+        except ValueError:
+            print("Please enter a valid number.")
+
+def mark_task_as_completed():
+    view_tasks()
+    if not tasks:
+        return
+    try:
+        num = int(input("Enter the number of the task you want to mark as completed: "))
+        if 1 <= num <= len(tasks):
+            tasks[num-1]["done"]=True
+            print(f"{num}. task is marked as completed.")
+        else:
+            print("invalid number.")
+    except ValueError:
+        print("Please enter a valid number.")
+
 
 
 while True:
@@ -19,50 +67,18 @@ while True:
         print("Please enter a valid number.")
         continue
 
-
     if choice == 1:
-        if not tasks:
-            time.sleep(0.3)
-            print("No tasks yet!")
-        else:
-            time.sleep(1)
-            print("\nYour tasks:")
-            for i, task in enumerate(tasks, start=1):
-                print(f"{i}. {task}")
-        pass
-
+        view_tasks()
 
     elif choice == 2:
-        new_task = input("Add a new task: ")
-        tasks.append(new_task)
-        time.sleep(1)
-        print("Task added!")
-        pass
-
+        add_task()
 
     elif choice == 3:
-        if not tasks:
-            print("No tasks to remove.")
-        else:
-            time.sleep(1)
-            print("\nYour tasks:")
-            for i, task in enumerate(tasks, start=1):
-                print(f"{i}. {task}")
-        try:
-            time.sleep(1)
-            delete_task = int(input("Enter the number of the task you want to delete: "))
-            if 1<= delete_task <= len(tasks):
-                removed = tasks.pop(delete_task-1)
-                time.sleep(1)
-                print(f"Removed: {removed}")
-            else:
-                print("invalid task number")
-        except ValueError:
-            print("Please enter a valid number.")
-            continue
-        pass
-
+        remove_task()
 
     elif choice == 4:
+        mark_task_as_completed()
+
+    elif choice == 5:
         print("Goodbye. Have a great day!")
         break
